843612959fd18e8bbf6744ccad317c5404cbd44c
HungryStudent <109438316+HungryStudent@users.noreply.github.com>
2023-04-13 18:09:57 +0300

    Fix

--- change_token.py
+++ change_token.py
@@ -5,6 +5,6 @@ from utils import db
 response = requests.post('https://iam.api.cloud.yandex.net/iam/v1/tokens',
                          headers={'Content-Type': 'application/json'},
                          json={
-                             "yandexPassportOauthToken": "y0_AgAAAABWCJPZAATuwQAAAADgwm_JZBZxXwkKT2--2959T82zdRs0O3c"
+                             "yandexPassportOauthToken":
                          })
 db.change_iam_token(response.json()['iamToken'])
