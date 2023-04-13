8b67e4470c61f779f0f32d4ec787c629fe741187
Nikita <nikita.efan.2017@gmail.com>
2023-04-13 23:49:36 +0300

    fix

--- change_token.py
+++ change_token.py
@@ -1,10 +1,11 @@
 import requests
 
+from config import ya_token
 from utils import db
 
 response = requests.post('https://iam.api.cloud.yandex.net/iam/v1/tokens',
                          headers={'Content-Type': 'application/json'},
                          json={
-                             "yandexPassportOauthToken":
+                             "yandexPassportOauthToken": ya_token
                          })
 db.change_iam_token(response.json()['iamToken'])
