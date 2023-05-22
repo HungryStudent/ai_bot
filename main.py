03cb83db2b715c2e478cdefffea9c46a9058342e
Nikita <nikita.efan.2017@gmail.com>
2023-05-23 00:46:38 +0300

    fix

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from handlers import users
 
 async def on_startup(_):
     db.start()
-    await bot.send_photo(796644977, "https://cdn.discordapp.com/attachments/808315295823167539/998727216462569572/unknown.png")
 
 
 if __name__ == "__main__":
